5334abb14feca60a7bdd8265a53f61621fee60c5
Himanshu Sinha <ashusinha1904@gmail.com>
2024-06-30 02:52:46 +0530

    New is-user-already-registered route added and some files renamed

--- index.ts
+++ index.ts
@@ -4,12 +4,13 @@ import cors from "cors";
 import { loginRouter } from "./routes/login-router";
 import { signupRouter } from "./routes/signup-router";
 import { verifyRouter } from "./routes/verify-router";
-import { addHeadRouter } from "./routes/add-head";
-import { addEventRouter } from "./routes/add-event";
-import { getEventsRouter } from "./routes/get-events";
+import { addHeadRouter } from "./routes/add-head-router";
+import { addEventRouter } from "./routes/add-event-router";
+import { getEventsRouter } from "./routes/get-events-router";
 import { registerForEventRouter } from "./routes/register-for-event-router";
 import { getMyEventsRouter } from "./routes/get-my-events-router";
 import { getHeadDetailsRouter } from "./routes/get-head-details-router";
+import { isUserAlreadyRegisteredRouter } from "./routes/is-user-already-registered-router";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -23,6 +24,7 @@ app.use("/api/get-events", getEventsRouter);
 app.use("/api/add-head", addHeadRouter);
 app.use("/api/add-event", addEventRouter);
 app.use("/api/register-for-event", registerForEventRouter);
+app.use("/api/is-user-already-registered", isUserAlreadyRegisteredRouter);
 app.use("/api/get-my-events", getMyEventsRouter);
 app.use("/api/get-head-details", getHeadDetailsRouter);
 
---
+++ routes/is-user-already-registered-router.ts
@@ -0,0 +1,52 @@
+import { Router } from "express";
+import jwt from "jsonwebtoken";
+import { ZodError } from "zod";
+
+import { registerValidator } from "../validators/user/register-for-event-validator";
+import prisma from "../libs/db";
+
+const isUserAlreadyRegisteredRouter = Router();
+
+isUserAlreadyRegisteredRouter.post("/", async (req, res) => {
+  try {
+    const { eventId, token } = await registerValidator.parseAsync(req.body);
+
+    const email = jwt.verify(token, process.env.JWT_SECRET!) as string;
+    if (!email) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    const user = await prisma.users.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    const isUserAlreadyRegistered = await prisma.usersAndEvents.findUnique({
+      where: {
+        eventId_userId: {
+          eventId: eventId,
+          userId: user.id,
+        },
+      },
+    });
+
+    return res
+      .status(200)
+      .json({ isUserAlreadyRegistered: !!isUserAlreadyRegistered });
+  } catch (error) {
+    if (error instanceof ZodError) {
+      res.status(422).json({ error: error.errors[0].message });
+    } else {
+      res
+        .status(500)
+        .json({ error: "Some error occured. Please try again later!" });
+    }
+  }
+});
+
+export { isUserAlreadyRegisteredRouter };
