8b687e6bcf63a67bbe4b7c6ae89d2f29ec802a19
Himanshu Sinha <ashusinha1904@gmail.com>
2024-07-15 22:45:17 +0530

    New route /delete-head added

--- index.ts
+++ index.ts
@@ -20,6 +20,7 @@ import { addScannerRouter } from "./routes/admin/add-scanner-router";
 import { userEntryRouter } from "./routes/scanner/user-entry-router";
 import { userExitRouter } from "./routes/scanner/user-exit-router";
 import { getAllHeadsRouter } from "./routes/admin/get-all-heads";
+import { deleteHeadRouter } from "./routes/admin/delete-head-router";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -45,6 +46,7 @@ app.use("/api/change-password", changePasswordRouter);
 app.use("/api/user-entry", userEntryRouter);
 app.use("/api/user-exit", userExitRouter);
 app.use("/api/get-all-heads", getAllHeadsRouter);
+app.use("/api/delete-head", deleteHeadRouter);
 
 app.get("/", (_, res) => {
   return res.send("Hello");
--- routes/admin/delete-head-router.ts
+++ routes/admin/delete-head-router.ts
@@ -1,10 +1,40 @@
 import { Router } from "express";
 import { ZodError } from "zod";
+import jwt from "jsonwebtoken";
+
+import prisma from "../../libs/db";
+import { deleteHeadValidator } from "../../validators/admin/delete-head-validator";
 
 const deleteHeadRouter = Router();
 
-deleteHeadRouter.post("/", (req, res) => {
+deleteHeadRouter.post("/", async (req, res) => {
   try {
+    const { id, token } = await deleteHeadValidator.parseAsync(req.body);
+    if (!token) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    const email = jwt.verify(token, process.env.JWT_SECRET!) as string;
+    if (!email) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    const admin = await prisma.admin.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (!admin) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    await prisma.heads.delete({
+      where: {
+        id,
+      },
+    });
+
+    return res.status(201).json({ message: "Head deleted successfully" });
   } catch (error) {
     if (error instanceof ZodError) {
       return res.status(422).json({ error: error.errors[0].message });
