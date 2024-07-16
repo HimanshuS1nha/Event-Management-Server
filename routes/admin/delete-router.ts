0b6deb739d0ac3a93126f601fe28107db53d0f9d
Himanshu Sinha <ashusinha1904@gmail.com>
2024-07-16 15:32:26 +0530

    New route /delete/event added

--- routes/admin/delete-router.ts
+++ routes/admin/delete-router.ts
@@ -85,4 +85,43 @@ deleteRouter.post("/head", async (req, res) => {
   }
 });
 
+deleteRouter.post("/event", async (req, res) => {
+  try {
+    const { id, token } = await deleteValidator.parseAsync(req.body);
+    if (!token) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    const email = jwt.verify(token, process.env.JWT_SECRET!) as string;
+    if (!email) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    const admin = await prisma.admin.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (!admin) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    await prisma.events.delete({
+      where: {
+        id,
+      },
+    });
+
+    return res.status(201).json({ message: "Head deleted successfully" });
+  } catch (error) {
+    if (error instanceof ZodError) {
+      return res.status(422).json({ error: error.errors[0].message });
+    } else {
+      return res
+        .status(500)
+        .json({ error: "Some error occured. Please try again later!" });
+    }
+  }
+});
+
 export { deleteRouter };
